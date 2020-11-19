42d0d6625eaa7e6fcd0316aa1408a50aa868189f
jiahao.feng <445786355@qq.com>
2020-11-19 19:48:02 +0800

    物品简易组件调整

--- demo/A.vue
+++ demo/A.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <ItemSimple :item="item1" />
+        <ItemSimple :item="item2" />
         <Article
             :content="content"
             directorybox="#directory"
@@ -11,14 +13,18 @@
 </template>
 
 <script>
+import ItemSimple from "../src/ItemSimple.vue";
 import Article from "../src/Article.vue";
 import demohtml from "./content";
+import {get_item} from "../service/item";
 export default {
     name: "A",
     props: [],
     data: function() {
         return {
             content: demohtml,
+            item1: null,
+            item2: null,
         };
     },
     computed: {},
@@ -36,9 +42,26 @@ export default {
         //         this.content = data.data.post.post_content
         //     })
         // });
+
+        get_item('10_310').then((res) => {
+            let data = res.data;
+            if (data.code === 200) {
+                let item = data.data.item;
+                this.item1 = JSON.stringify(item) !== "{}" ? item : null;
+            }
+        });
+
+        get_item('6_4396').then((res) => {
+            let data = res.data;
+            if (data.code === 200) {
+                let item = data.data.item;
+                this.item2 = JSON.stringify(item) !== "{}" ? item : null;
+            }
+        });
     },
     components: {
         Article,
+        ItemSimple,
     },
 };
 </script>
--- src/ItemSimple.vue
+++ src/ItemSimple.vue
@@ -1,25 +1,48 @@
 <template>
-  <div class="m-simple-item">
-    <img class="u-icon" :src="icon_url(item.IconID)" alt="`IconID:${item.IconID}`">
-    <span class="u-name" :style="{'color':item_color(item.Quality)}" v-text="item.Name"></span>
-    <span class="u-uiid fr" v-text="`ID:${item.id}`"></span>
-  </div>
+  <el-popover v-if="item"
+              placement="bottom-start"
+              popper-class="m-simple-item-popup"
+              width="auto"
+              :visible-arrow="false"
+              trigger="hover"
+              transition="none"
+              :close-delay="0"
+  >
+    <div class="m-simple-item" slot="reference">
+      <img class="u-icon" :src="icon_url(item.IconID)" alt="`IconID:${item.IconID}`">
+      <span class="u-name" :style="{'color':item_color(item.Quality)}" v-text="item.Name"></span>
+      <span class="u-uiid fr" v-text="`ID:${item.id}`"></span>
+    </div>
+    <jx3-item :item_id="item.id"/>
+  </el-popover>
 </template>
 
 <script>
-import icon_url from '../assets/js/item/icon_url.js';
-import item_color from '../assets/js/item/color.js';
+  import Item from "./Item";
+  import icon_url from '../assets/js/item/icon_url.js';
+  import item_color from '../assets/js/item/color.js';
 
-export default {
+  export default {
     name: "ItemSimple",
     props: ["item"],
     methods: {
       icon_url,
       item_color,
     },
-};
+    components: {
+      "jx3-item": Item,
+    },
+  };
 </script>
 
-<style scoped lang="less">
-@import "../assets/css/module/item_simple.less";
+<style lang="less">
+  .m-simple-item-popup {
+    padding: 0;
+    border: none;
+    box-shadow: none;
+    transform: translateY(-10px);
+  }
+</style>
+<style lang="less" scoped>
+  @import "../assets/css/module/item_simple.less";
 </style>
