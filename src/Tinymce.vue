91537807ab9da1a1693df9ebe29c868e01ee42e3
iRuxu <rx6@qq.com>
2021-08-01 00:52:23 +0800

    add talent2

--- src/Tinymce.vue
+++ src/Tinymce.vue
@@ -65,18 +65,18 @@ export default {
                 plugins: [
                     "link autolink",
                     "hr lists advlist table codeinline codesample checklist foldtext latex",
-                    "image emoticons media videox macro qixue",
+                    "image emoticons media videox macro qixue talent2",
                     "autosave code fullscreen wordcount powerpaste pagebreak printpage", // template anchor jx3icon
                 ],
                 toolbar: [
                     "undo | formatselect | fontsizeselect | forecolor backcolor | bold italic underline strikethrough superscript subscript | link unlink | restoredraft fullscreen code",
-                    "removeformat | hr alignleft aligncenter alignright alignjustify indent outdent | bullist numlist checklist table blockquote foldtext codeinline codesample latex | emoticons image media videox | macro qixue pagebreak printpage", // template anchor jx3icon
+                    "removeformat | hr alignleft aligncenter alignright alignjustify indent outdent | bullist numlist checklist table blockquote foldtext codeinline codesample latex | emoticons image media videox | macro qixue talent2 pagebreak printpage", // template anchor jx3icon
                 ],
                 mobile: {
                     toolbar_drawer: true,
                     toolbar: [
                         "undo emoticons bold forecolor backcolor removeformat pagebreak fullscreen",
-                        "hr alignleft aligncenter alignright alignjustify indent outdent bullist numlist checklist table blockquote codesample latex macro qixue media",
+                        "hr alignleft aligncenter alignright alignjustify indent outdent bullist numlist checklist table blockquote codesample latex macro qixue talent2 media",
                     ],
                 },
                 block_formats:
