cdc2ac1239d5a44935e02f4b08ad0f6b0044f92d
iRuxu <rx6@qq.com>
2021-12-25 02:12:09 +0800

    fix:npc style

--- src/Npc.vue
+++ src/Npc.vue
@@ -8,9 +8,7 @@
                         <span class="u-name-add">
                             <span v-if="data.Title || data.Level">&lt;</span>
                             <span class="u-nick" v-if="data.Title">
-                                {{
-                                data.Title
-                                }}
+                                {{ data.Title }}
                             </span>
                             <span v-if="data.Title && data.Level">·</span>
                             <b class="u-level" v-if="data.Level">{{ data.Level }}</b>
@@ -18,183 +16,144 @@
                         </span>
                         <div class="u-base">
                             <span class="u-atmap" v-if="data.MapName">
-                                <!-- <em>MapName</em> -->
                                 {{ data.MapName }}
                             </span>
-                            <!-- <span class="u-totation" v-if="data._Notation">
-                                <em>_Notation</em>
-                                {{ data._Notation }}
-                            </span>-->
                             <span class="u-intensity" v-if="data.Intensity">
                                 强度
-                                <!-- <em>Intensity</em> -->
                                 {{ data.Intensity }}
                             </span>
                         </div>
                     </div>
                     <div class="u-primary">
                         <div class="u-life">
-                            <img
-                                class="u-descicon u-descicon-life"
-                                svg-inline
-                                src="../assets/img/npc/energy.svg"
-                            />
+                            <img class="u-descicon u-descicon-life" svg-inline src="../assets/img/npc/energy.svg" />
                             <b>血量</b>
                             <em>MaxLife</em>
                             <strong>{{ data.MaxLife }}</strong>
                         </div>
                         <div class="u-mana">
-                            <img
-                                class="u-descicon u-descicon-mana"
-                                svg-inline
-                                src="../assets/img/npc/energy.svg"
-                            />
+                            <img class="u-descicon u-descicon-mana" svg-inline src="../assets/img/npc/energy.svg" />
                             <b>内力</b>
                             <em>MaxMana</em>
                             <strong>{{ data.MaxMana }}</strong>
                         </div>
                         <div class="u-touch" v-if="data.TouchRange">
-                            <img
-                                class="u-descicon u-descicon-touch"
-                                svg-inline
-                                src="../assets/img/npc/radar.svg"
-                            />
+                            <img class="u-descicon u-descicon-touch" svg-inline src="../assets/img/npc/radar.svg" />
                             <b>范围</b>
                             <em>TouchRange</em>
                             <strong>{{ data.TouchRange }}</strong>
                         </div>
                         <div class="u-sense" v-if="data.Sense">
-                            <img
-                                class="u-descicon u-descicon-sense"
-                                svg-inline
-                                src="../assets/img/npc/sight.svg"
-                            />
+                            <img class="u-descicon u-descicon-sense" svg-inline src="../assets/img/npc/sight.svg" />
                             <b>识破</b>
                             <em>Sense</em>
                             <strong>{{ data.Sense }}</strong>
                         </div>
                         <div class="u-dodge" v-if="data.Dodge">
-                            <img
-                                class="u-descicon u-descicon-dodge"
-                                svg-inline
-                                src="../assets/img/npc/miss.svg"
-                            />
+                            <img class="u-descicon u-descicon-dodge" svg-inline src="../assets/img/npc/miss.svg" />
                             <b>闪避</b>
                             <em>Dodge</em>
                             <strong>{{ data.Dodge }}</strong>
                         </div>
                         <div class="u-dodge" v-if="data.Parry">
-                            <img
-                                class="u-descicon u-descicon-dodge"
-                                svg-inline
-                                src="../assets/img/npc/miss.svg"
-                            />
+                            <img class="u-descicon u-descicon-dodge" svg-inline src="../assets/img/npc/miss.svg" />
                             <b>招架</b>
                             <em>Parry</em>
                             <strong>{{ data.ParryValue }}</strong>
                         </div>
                         <div class="u-shield">
-                            <img
-                                class="u-descicon u-descicon-shield"
-                                svg-inline
-                                src="../assets/img/npc/shield.svg"
-                            />
+                            <img class="u-descicon u-descicon-shield" svg-inline src="../assets/img/npc/shield.svg" />
                             <b>防御</b>
                             <em>Shield</em>
                             <span class="u-sitem">
                                 外功防御
                                 <em>PhysicsShieldBase</em>
-                                <span class="u-value">{{~~data.PhysicsShieldBase}}</span>
+                                <span class="u-value">{{ ~~data.PhysicsShieldBase }}</span>
                             </span>
                             <span class="u-sitem">
                                 混元防御
                                 <em>NeutralMagicDefence</em>
-                                <span class="u-value">{{~~data.NeutralMagicDefence}}</span>
+                                <span class="u-value">{{ ~~data.NeutralMagicDefence }}</span>
                             </span>
                             <span class="u-sitem">
                                 阳性防御
                                 <em>SolarMagicDefence</em>
-                                <span class="u-value">{{~~data.SolarMagicDefence}}</span>
+                                <span class="u-value">{{ ~~data.SolarMagicDefence }}</span>
                             </span>
                             <span class="u-sitem">
                                 阴性防御
                                 <em>LunarMagicDefence</em>
-                                <span class="u-value">{{~~data.LunarMagicDefence}}</span>
+                                <span class="u-value">{{ ~~data.LunarMagicDefence }}</span>
                             </span>
                             <span class="u-sitem">
                                 毒性防御
                                 <em>PoisonMagicDefence</em>
-                                <span class="u-value">{{~~data.PoisonMagicDefence}}</span>
+                                <span class="u-value">{{ ~~data.PoisonMagicDefence }}</span>
                             </span>
                         </div>
                         <div class="u-critical">
-                            <img
-                                class="u-descicon u-descicon-attack"
-                                svg-inline
-                                src="../assets/img/npc/attack.svg"
-                            />
+                            <img class="u-descicon u-descicon-attack" svg-inline src="../assets/img/npc/attack.svg" />
                             <b>会心</b>
                             <em>Critical</em>
                             <span class="u-sitem">
                                 外功会心
                                 <em>PhysicsCriticalStrike</em>
-                                <span class="u-value">{{~~data.PhysicsCriticalStrike}}</span>
+                                <span class="u-value">{{ ~~data.PhysicsCriticalStrike }}</span>
                             </span>
                             <span class="u-sitem">
                                 混元会心
                                 <em>NeutralCriticalStrike</em>
-                                <span class="u-value">{{~~data.NeutralCriticalStrike}}</span>
+                                <span class="u-value">{{ ~~data.NeutralCriticalStrike }}</span>
                             </span>
                             <span class="u-sitem">
                                 阳性会心
                                 <em>SolarCriticalStrike</em>
-                                <span class="u-value">{{~~data.SolarCriticalStrike}}</span>
+                                <span class="u-value">{{ ~~data.SolarCriticalStrike }}</span>
                             </span>
                             <span class="u-sitem">
                                 阴性会心
                                 <em>LunarCriticalStrike</em>
-                                <span class="u-value">{{~~data.LunarCriticalStrike}}</span>
+                                <span class="u-value">{{ ~~data.LunarCriticalStrike }}</span>
                             </span>
                             <span class="u-sitem">
                                 毒性会心
                                 <em>PoisonCriticalStrike</em>
-                                <span class="u-value">{{~~data.PoisonCriticalStrike}}</span>
+                                <span class="u-value">{{ ~~data.PoisonCriticalStrike }}</span>
                             </span>
                         </div>
                         <div class="u-attack">
-                            <img
-                                class="u-descicon u-descicon-attack"
-                                svg-inline
-                                src="../assets/img/npc/target.svg"
-                            />
+                            <img class="u-descicon u-descicon-attack" svg-inline src="../assets/img/npc/target.svg" />
                             <b>命中</b>
                             <em>Attack</em>
                             <span class="u-sitem">
                                 外功命中
                                 <em>PhysicsAttackHit</em>
-                                <span class="u-value">{{~~data.PhysicsAttackHit}}</span>
+                                <span class="u-value">{{ ~~data.PhysicsAttackHit }}</span>
                             </span>
                             <span class="u-sitem">
                                 混元命中
                                 <em>NeutralMagicHit</em>
-                                <span class="u-value">{{~~data.NeutralMagicHit}}</span>
+                                <span class="u-value">{{ ~~data.NeutralMagicHit }}</span>
                             </span>
                             <span class="u-sitem">
                                 阳性命中
                                 <em>SolarMagicHit</em>
-                                <span class="u-value">{{~~data.SolarMagicHit}}</span>
+                                <span class="u-value">{{ ~~data.SolarMagicHit }}</span>
                             </span>
                             <span class="u-sitem">
                                 阴性命中
                                 <em>LunarMagicHit</em>
-                                <span class="u-value">{{~~data.LunarMagicHit}}</span>
+                                <span class="u-value">{{ ~~data.LunarMagicHit }}</span>
                             </span>
                             <span class="u-sitem">
                                 毒性命中
                                 <em>PoisonMagicHit</em>
-                                <span class="u-value">{{~~data.PoisonMagicHit}}</span>
+                                <span class="u-value">{{ ~~data.PoisonMagicHit }}</span>
                             </span>
                         </div>
+                    </div>
+                    <div class="u-id">
+                        ID : {{id}}
                     </div>
                 </div>
             </div>
@@ -205,7 +164,6 @@
 <script>
 import { getNpc } from "../service/database.js";
 import { iconLink } from "@jx3box/jx3box-common/js/utils";
-import detach_types from "../assets/data/detach_type.json";
 export default {
     name: "Npc",
     props: ["client", "id"],
@@ -213,7 +171,7 @@ export default {
         data: null,
     }),
     computed: {
-        params: function () {
+        params: function() {
             return [this.client, this.id];
         },
     },
@@ -225,9 +183,7 @@ export default {
                 if (val) {
                     let [client, id] = val;
                     // 读取本地数据
-                    const cache = sessionStorage.getItem(
-                        `npc-${client}-${id}`
-                    );
+                    const cache = sessionStorage.getItem(`npc-${client}-${id}`);
                     if (cache) {
                         this.data = JSON.parse(cache);
                         // 没有缓存则发起请求获取数据
@@ -238,10 +194,7 @@ export default {
                                 this.data = data;
 
                                 // 将数据放入 sessionStorage
-                                sessionStorage.setItem(
-                                    `npc-${client}-${id}`,
-                                    JSON.stringify(data)
-                                );
+                                sessionStorage.setItem(`npc-${client}-${id}`, JSON.stringify(data));
                             });
                     }
                 }
@@ -249,19 +202,10 @@ export default {
         },
     },
     methods: {
-        iconLink: function (id) {
+        iconLink: function(id) {
             return iconLink(id, this.client);
         },
     },
-    filters : {
-        showDetachType: function (val) {
-            if (val && detach_types[val]) {
-                return detach_types[val];
-            } else {
-                return "";
-            }
-        },
-    }
 };
 </script>
 
