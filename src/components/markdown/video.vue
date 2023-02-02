a04e062594a0d08f8d9c32768c62a72b0f191c44
Kaviilee <jiaweilee95@126.com>
2023-02-03 00:53:52 +0800

    feat: markdown video

--- src/components/markdown/video.vue
+++ src/components/markdown/video.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="c-editor-markdown-dialog">
-        <span class="c-markdown-toolbar-video c-markdown-toolbar-item" @click="handleClick" title="插入视频">
+        <span
+            class="c-markdown-toolbar-video c-markdown-toolbar-item"
+            @click="handleClick"
+            title="插入视频"
+        >
             <i class="el-icon-video-camera"></i>
         </span>
 
-        <el-dialog :visible.sync="visible" title="插入视频" :modal-append-to-body="false">
-            <div class="u-help">请填入BV号或链接地址。<a style="color: #0366D6;font-weight: normal;" href="/tool/686/" target="_blank">[点击查看帮助]</a></div>
+        <el-dialog
+            :visible.sync="visible"
+            title="插入视频"
+            :modal-append-to-body="false"
+        >
+            <div class="u-help">
+                请填入BV号或链接地址。<a
+                    style="color: #0366d6; font-weight: normal"
+                    href="/tool/686/"
+                    target="_blank"
+                    >[点击查看帮助]</a
+                >
+            </div>
 
-            <el-input class="u-input" placeholder="请输入BV号或链接地址" v-model="videoUrl"></el-input>
+            <el-input
+                class="u-input"
+                placeholder="请输入BV号或链接地址"
+                v-model="videoUrl"
+            ></el-input>
 
             <div slot="footer">
                 <el-button @click="cancel">取消</el-button>
@@ -35,10 +54,21 @@ export default {
             this.videoUrl = "";
         },
         insert() {
-            const content = `<iframe class="w-player-bilibili" src="//player.bilibili.com/player.html?bvid=${this.videoUrl}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="400" height="300"> </iframe>`;
+            let videox = this.videoUrl;
+            if (videox.startsWith("https://www.bilibili.com/video/")) {
+                const regex = /https:\/\/www.bilibili.com\/video\/(BV\w+)/;
+                const found = videox.match(regex);
+                if (found) {
+                    videox = found[1];
+                }
+            }
+            if (!videox) {
+                alert("请输入BV号");
+                return;
+            }
+            const content = `<iframe class="w-player-bilibili" src="//player.bilibili.com/player.html?bvid=${videox}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" width="400" height="300"> </iframe>`;
 
             this.$emit("insert", content);
-
 
             this.videoUrl = "";
 
