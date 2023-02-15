83aca6f6881f9b9265d10bff28101e78d8616441
Kaviilee <jiaweilee95@126.com>
2023-02-15 16:34:53 +0800

    fix: cdn replace

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,5 +1,6 @@
 // 图片地址&懒加载
 import { resolveImagePath } from "@jx3box/jx3box-common/js/utils";
+import { __cdn } from "@jx3box/jx3box-common/data/jx3box.json";
 function lazyLoad(str) {
     if (!str) return;
 
@@ -9,6 +10,9 @@ function lazyLoad(str) {
         let origin = item[0];
         let prefix = item[1];
         let src = resolveImagePath(item[2]);
+        if (src.includes('https://console.cnyixun.com/')) {
+            src = src.replace('https://console.cnyixun.com/', __cdn)
+        }
         let suffix = item[3];
         let output = `<img ${prefix} loading="lazy" src="${src}" ${suffix}>`;
         str = str.replace(origin, output);
