7c043ff4ad5b18974bca6f286b8cfc61b94a8877
Sloths <40552272+zixuan1986@users.noreply.github.com>
2023-11-29 11:08:57 +0800

    letter (#22)
    * feat: letter
    
    * feat: letter
    
    * perf: alert
    
    * fix: letter
    
    * fix: letter
    
    * feat:letter
    
    * del: letter.js
    
    * perf: style
    
    * fix：style

---
+++ src/components/Letter.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="e-letter" v-html="html"></div>
+</template>
+
+<script>
+export default {
+	name: "Letter",
+	emits: ["update"],
+	props: {
+		data: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	data() {
+		return {
+			html: "",
+		};
+	},
+	watch: {
+		data: {
+			immediate: true,
+			deep: true,
+			handler(obj) {
+				this.html = this.htmlDemo(obj);
+			},
+		},
+		html: {
+			immediate: true,
+			handler(val) {
+				this.$emit("update", { id: this.data.id, html: val });
+			},
+		},
+	},
+	methods: {
+		// 默认html
+		htmlDemo({ slug, style }) {
+			const titleHtml = `<div class="e-letter-title letter-title--${slug}">我是抬头</div>`;
+			const contentHtml = `<div class="e-letter-content letter-header--${slug}"><div class="u-letter-content--header letter-footer--${slug}"><div class="u-letter-content--footer letter-body--${slug}"><p>明月几时有，把酒问青天。</p></div></div></div>`;
+			return `<div class="e-letter letter--${slug}">${titleHtml}<br/> ${contentHtml}</div></div><style>${style}</style>`;
+		},
+	},
+};
+</script>
