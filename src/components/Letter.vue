4893a5bfe83d0f52afd2f371fbbc9bf3cd6b9a2d
litthy <litthy@qq.com>
2023-11-29 17:34:22 +0800

    fix: html

--- src/components/Letter.vue
+++ src/components/Letter.vue
@@ -29,7 +29,7 @@ export default {
 		// 默认html
 		htmlDemo({ slug, style }) {
 			const titleHtml = `<div class="e-letter-title letter-title--${slug}">我是标题</div>`;
-			const contentHtml = `<div class="e-letter-content letter-header--${slug}"><div class="u-letter-content--header letter-footer--${slug}"><div class="u-letter-content--footer letter-body--${slug}"><p>明月几时有，把酒问青天。</p></div></div></div>`;
+            const contentHtml = `<div class="e-letter-content letter-body--${slug}"><div class="u-letter-content--header letter-header--${slug}"><div class="u-letter-content--footer letter-footer--${slug}"><p contenteditable="true">明月几时有，把酒问青天。</p></div></div></div>`;
 			return `<div class="e-letter letter--${slug}">${titleHtml}<br/> ${contentHtml}</div></div><style>${style}</style>`;
 		},
 	},
